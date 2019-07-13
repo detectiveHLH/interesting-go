714c48a6172d24bdad234faf242d324d66f62091
hulunhao <545759184@qq.com>
2019-07-13 18:04:34 +0800

    Add travel_directory.go

--- travel_directory.go
+++ travel_directory.go
@@ -1,10 +1,54 @@
 package main
 
-import "os"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 func main() {
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	fileSize := make(chan int64)
+	go func() {
+		for _, root := range roots {
+			travel(root, fileSize)
+		}
+		close(fileSize)
+	}()
+
+
+	// show the result
+	var nfiles, nbytes int64
+	for size := range fileSize {
+		nfiles++
+		nbytes += size
+	}
+	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+func travel(dir string, fileSize chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			travel(subdir, fileSize)
+		} else {
+			fileSize <- entry.Size()
+		}
+	}
 }
 
 func dirents(dir string) []os.FileInfo {
-	entries, err := ioutil.Read
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Read dir err: %v\n", err)
+		return nil
+	}
+	return entries
 }
